338393adf5ef6942fe1ba1e2b23ee9e00965bd2c
PiotrMag <piotr_mag@wp.pl>
2020-04-29 18:55:52 +0200

    Dodaj pliki projektów.

---
+++ POBankApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace POBankApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
