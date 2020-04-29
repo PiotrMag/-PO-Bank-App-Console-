a4639b78a53a04052060af574644913bc38e5396
PiotrMag <piotr_mag@wp.pl>
2020-04-29 19:06:04 +0200

    Pierwszy commit

--- POBankApp/Program.cs
+++ POBankApp/Program.cs
@@ -6,7 +6,7 @@ namespace POBankApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Pierwszy commit");
         }
     }
 }
