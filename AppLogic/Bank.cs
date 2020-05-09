d90da0a686fd530781c94a8042075a80d88c5045
PiotrMag <piotr_mag@wp.pl>
2020-05-09 14:23:23 +0200

    Zmieniono AuthenticationResult na BankActionResult (po to, aby mozna bylo uzywac jednego enum do roznych rodzai akcji wykonywancyh w banku). Dodano liste kart do klasy Bank. Dodano dokumentacje do konstruktora klasy Bank. Dodano metode AddCard do klasy Bank. Zmieniono AuthenticationResult.ACCEPTED na BankActionResult.SUCCESS

--- AppLogic/Bank.cs
+++ AppLogic/Bank.cs
@@ -6,16 +6,23 @@ using System.Threading.Tasks;
 
 namespace AppLogic
 {
-    public enum AuthorizationResult
+    public enum BankActionResult
     {
         NULL = -1,
-        ACCEPTED = 0,
-        REJECTED_NO_SUCH_USER = 1,
+        SUCCESS = 0,
+        REJECTED_CANT_ADD_CARD = 1,
+        REJECTED_NO_SUCH_USER = 2,
     }
 
     public class Bank
     {
         public String Name { get; set; }
+        public List<Card> cards { get; }
+
+        /// <summary>
+        /// Konstruktor obiektu Bank
+        /// </summary>
+        /// <param name="name">Nazwa banku</param>
         public Bank(String name)
         {
             this.Name = name;
@@ -25,11 +32,23 @@ namespace AppLogic
         /// Probuje autoryzowac karte i dokonac platnosci
         /// </summary>
         /// <returns>
-        /// Zwraca AuthorizationResult, ktory mowi o tym, czy autoryzacja sie powiodla, czy nie
+        /// BankActionResult, ktory mowi o tym, czy autoryzacja sie powiodla, czy nie
         /// </returns>
-        public AuthorizationResult Authorize ()
+        public BankActionResult Authorize ()
         {
-            return AuthorizationResult.NULL;
+            return BankActionResult.NULL;
+        }
+
+        /// <summary>
+        /// Dodaj nowa/istniejaca karte do banku
+        /// </summary>
+        /// <param name="card">Karta do dodania</param>
+        /// <returns>
+        /// BankActionResult, ktory mowi o tym, czy akcja sie powiodla, czy nie
+        /// </returns>
+        public BankActionResult AddCard(Card card)
+        {
+            return BankActionResult.NULL;
         }
     }
 }
