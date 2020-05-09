f9c69d6a51a4954685ff537f1531eea93586077b
PiotrMag <piotr_mag@wp.pl>
2020-05-09 13:58:56 +0200

    Dodano metode Authorize do klasy Bank. Na razie nie przyjmuje ona argumentow. Dodano enum  AuthorizationResult

--- AppLogic/Bank.cs
+++ AppLogic/Bank.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace AppLogic
 {
+    public enum AuthorizationResult
+    {
+        NULL = -1,
+        ACCEPTED = 0,
+        REJECTED_NO_SUCH_USER = 1,
+    }
+
     public class Bank
     {
+        public String Name { get; set; }
+        public Bank(String name)
+        {
+            this.Name = name;
+        }
+
+        public AuthorizationResult Authorize ()
+        {
+            return AuthorizationResult.NULL;
+        }
     }
 }
